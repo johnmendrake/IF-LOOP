2fe3481778bd540ae065cc07ff9a14d96290bcfd
sarius <sarius@erebos.fritz.box>
2019-06-26 10:02:44 +0200

    added method heads and raw structure

--- src/ifloop/Main.java
+++ src/ifloop/Main.java
@@ -1,10 +1,50 @@
 package ifloop;
 
+/**
+ * Dies wird die Steuerungsklasse des Projekts. In ihr werden die erforderlichen
+ * Stundenpläne und Verknüpfungen zwischen diesen in Form eines Graphen
+ * gespeichert. Der Suchmechanismus nach dem kürzesten Weg findet ebenfalls in
+ * dieser Klasse seine Implementierung und das Ergebnis wird für die
+ * Kommandozeile optimiert ausgegeben.
+ * 
+ * @author sarius
+ *
+ */
+
 public class Main {
 
+	/**
+	 * Hier wird der gesamte Ablauf geregelt. Die Methode soll optional mit arg[0]
+	 * einen Startpunkt der Informationskette übergeben bekommen (Ausgangspunkt Algorithmus von
+	 * Dijkstra).
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	/**
+	 * Kürzester Weg von einem Knoten zu einem bestimmten anderen.
+	 * @param pVertex
+	 */
+	public static void shortestWayToVertex(Vertex startVertex, Vertex targetVertex) {
+		// change return type
+	}
+	
+	/**
+	 * Liste kürzester Wege zu allen Knoten von einem Ausgangsknoten.
+	 */
+	public static void shortestWayToVertexes(Vertex startVertex) {
+		// change return type
+	}
+	
+	/**
+	 * Finde den minimalen Stammbaum nach Prim.
+	 */
+	public static void prim() {
+		
 	}
 
 }
